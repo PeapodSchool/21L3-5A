6f70fdb827e65fea61a879d20b62f17542b9322c
PeapodSchool <ppmailstudy@gmail.com>
2022-02-19 09:16:19 +0200

    Lesson 1

---
+++ app/src/main/java/com/peapod/crowcounter/MainActivity.java
@@ -0,0 +1,22 @@
+package com.peapod.crowcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView countText;
+    int crows = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void count(View view) {
+    }
+}
